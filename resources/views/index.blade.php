88e15890cc1c32efd4389d2815776dffa1694b2a
Agung Wicaksono <agungthebesttop@gmail.com>
2019-12-25 11:46:37 +0700

    CRUD Laravel
    Daftar Mahasiswa dengan CRUD

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/main')
+
+@section('title', 'Web Agung')
+
+@section('container')
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+        <h1 class="mt-2">Hello, world!</h1>
+    </div>
+  </div>
+<div>
+
+@endsection
